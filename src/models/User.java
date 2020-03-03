143893b2d3a66351e6b6d02442c661b597166c39
HamzaJadid <hamzah.jadeed@gmail.com>
2020-03-04 00:18:10 +0200

    First commit

---
+++ src/models/User.java
@@ -0,0 +1,79 @@
+package models;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
+@DatabaseTable(tableName = "user")
+public class User {
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField
+    private String firstName;
+    @DatabaseField
+    private String lastName;
+    @DatabaseField
+    private String email;
+    @DatabaseField
+    private String password;
+    @DatabaseField
+    private String phone;
+    @DatabaseField(dataType = DataType.DATE)
+    private Date dateOfBirth;
+
+    public User(){ }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
